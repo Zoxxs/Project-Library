79297be21cb04ce4efc4becdc2f464bd334ae40b
Zoxxs <136922170+Zoxxs@users.noreply.github.com>
2023-10-24 20:37:26 +0200

    Add function for making a book from user input

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ const theHobbit = new Book(`The Hobbit`, `J.R.R. Tolkien`, 295, "yep")
 function addBookToLibrary(newBook) {
   myLibrary.push(newBook);
 }
+
+const getBookFromInput = () => {
+  const bookInput = document.getElementById('book-name').value;
+  const authorInput = document.getElementById('book-author').value;
+  const pagesInput = document.getElementById('pages').value;
+  return new Book(bookInput, authorInput, pagesInput,)
+}
+
+const addNewBook = (e) => {
+  e.preventDefault();
+  const newBook = getBookFromInput();
+  addBookToLibrary(newBook);
+  console.log(myLibrary);
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', addNewBook);
